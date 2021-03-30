3fa5d2a58d8d2d41d4f73e841bf6cc3fde8dc95a
dkdk15 <77323428+dkdk15@users.noreply.github.com>
2021-03-30 11:56:34 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,16 @@
+<p>Гитарное оборудование</p>
+<p><?php echo htmlspecialchars($_POST['prm1']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm2']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm3']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm4']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm5']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm6']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm7']); ?>.</p>
+<p>клавишное оборудование</p>
+<p><?php echo htmlspecialchars($_POST['prm8']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm9']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm10']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm11']); ?>.</p>
+<p><?php echo htmlspecialchars($_POST['prm12']); ?>.</p>
+<p>контактная информация</p>
+<p><?php echo htmlspecialchars($_POST['prm13']); ?>.</p>
